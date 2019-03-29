7fe11d676c55d21980b982dacc535a213c24b1d0
PaDuckk <kang10019@naver.com>
2019-03-29 17:07:45 +0900

    init.js 파일 이름수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import { MyMNA, MNA, Party, Bill } from "./pages";
 import Menu from "./components/Menu";
+import { MyMNA, MNA, Party, Bill } from "./pages";
 import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
