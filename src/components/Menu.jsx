33ad8428f730aac7e994c0fef687622ac4294056
PaDuckk <kang10019@naver.com>
2019-03-30 11:43:16 +0900

    Add Styling Bootstrap

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,25 +3,51 @@ import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/">홈</NavLink>
-        </li>
-        <li>
-          <NavLink to="/mymna">내 의원</NavLink>
-        </li>
-        <li>
-          <NavLink to="/mna">의원</NavLink>
-        </li>
-        <li>
-          <NavLink to="/party">정당</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bill">법안</NavLink>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/">
+        국회 Watcher
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/mymna">
+              내 의원
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/mna">
+              의원
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/party">
+              정당
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/bill">
+              법안
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="navbar-nav mt-2 mt-lg-0">
+          <li className="nav-item">
+            <i className="fas fa-user"> 로그인</i>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
