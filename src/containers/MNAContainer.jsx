2a31730ed9c02c513b0ca586e47c3dba9f1fb80a
PaDuckk <kang10019@naver.com>
2019-04-04 11:00:59 +0900

    making data function separate

--- src/containers/MNAContainer.jsx
+++ src/containers/MNAContainer.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import OptionHistogramChart from "../components/OptionHistogramChart";
 import MNARankingList from "../components/MNARankingList";
-
+import makeHistogramChartData from "../lib/makeHistogramChartData";
 export default class MNAContainer extends Component {
   state = {
     mnaRankList: [
@@ -44,57 +44,10 @@ export default class MNAContainer extends Component {
   };
 
   render() {
-    function makeChartData(data, groupBy) {
-      const result = [];
-      result["attendanceRate"] = [];
-      result["billCount"] = [];
-
-      const billCountYDomain = data.reduce(
-        (acc, curr) => {
-          if (curr.billCount < acc.min) {
-            acc.min = curr.billCount;
-          }
-          if (curr.billCount > acc.max) {
-            acc.max = curr.billCount;
-          }
-          return acc;
-        },
-        { min: Infinity, max: 0 }
-      );
-      console.log(billCountYDomain);
-      for (let i = 1; i < 11; i++) {
-        result["attendanceRate"].push({ name: `~ ${i}0%` });
-        result["billCount"].push({
-          name: `~ ${Math.floor(
-            ((billCountYDomain.max - billCountYDomain.min) / 10) * i
-          ) + billCountYDomain.min}`
-        });
-      }
-
-      data.forEach(data => {
-        const attendanceIndex = Math.floor(data.attendanceRate / 10) - 1;
-        let billIndex =
-          Math.floor(
-            data.billCount /
-              ((billCountYDomain.max - billCountYDomain.min) / 10)
-          ) - 1;
-        if (billIndex < 0) billIndex++;
-        result["attendanceRate"][attendanceIndex]["count"] =
-          result["attendanceRate"][attendanceIndex]["count"] || 0;
-        result["attendanceRate"][attendanceIndex]["count"]++;
-
-        result["billCount"][billIndex]["count"] =
-          result["billCount"][billIndex]["count"] || 0;
-        result["billCount"][billIndex]["count"]++;
-      });
-      console.log(result);
-      return result;
-    }
-
     return (
       <div>
         <OptionHistogramChart
-          data={makeChartData(this.state.mnaRankList)}
+          data={makeHistogramChartData(this.state.mnaRankList)}
           options={[
             { key: "attendanceRate", comment: "출석률" },
             { key: "billCount", comment: "의안 제출 수" }
