b812cc317e42d1726fa4fdc07d90863027b4b536
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-06 08:19:01 +0900

    pieChart color option added

--- src/containers/MNAContainer.jsx
+++ src/containers/MNAContainer.jsx
@@ -1,31 +1,67 @@
 import React, { Component } from "react";
 import OptionHistogramChart from "../components/OptionHistogramChart";
 import MNARankingList from "../components/MNARankingList";
-import makeHistogramChartData from "../lib/makeHistogramChartData";
+import HistogramChartWrap from "../components/HistogramChartWrap";
 import { mnaData } from "./fakedata";
 
 export default class MNAContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mnaRankList: null
+      mnaList: null,
+      histoDataType: 0
     };
+
+    this.onClickAttendanceRateButton = this.onClickAttendanceRateButton.bind(
+      this
+    );
+    this.onClickBillSubmitCountButton = this.onClickBillSubmitCountButton.bind(
+      this
+    );
   }
 
   componentDidMount() {
-    this.setState({ mnaRankList: mnaData });
+    this.setState({ mnaList: mnaData, histoDataType: 0 });
   }
+
+  onClickAttendanceRateButton() {
+    this.setState({ histoDataType: 0 });
+  }
+
+  onClickBillSubmitCountButton() {
+    this.setState({ histoDataType: 1 });
+  }
+
   render() {
-    return this.state.mnaRankList ? (
+    if (this.state.mnaList) {
+      var data =
+        this.state.histoDataType === 0
+          ? this.state.mnaList.map(mna => mna.attendanceRate)
+          : this.state.mnaList.map(mna => mna.billCount);
+    }
+
+    return this.state.mnaList ? (
       <div>
-        <OptionHistogramChart
-          data={makeHistogramChartData(this.state.mnaRankList)}
-          options={[
-            { key: "attendanceRate", comment: "출석률" },
-            { key: "billCount", comment: "의안 제출 수" }
-          ]}
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.onClickAttendanceRateButton}
+        >
+          출석률
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.onClickBillSubmitCountButton}
+        >
+          법안제출
+        </button>
+        <HistogramChartWrap
+          dataSet={data}
+          chartName="의원 분포"
+          name="histogram"
         />
-        <MNARankingList data={this.state.mnaRankList} />
+        <MNARankingList data={this.state.mnaList} />
       </div>
     ) : (
       <div>now loading...</div>
