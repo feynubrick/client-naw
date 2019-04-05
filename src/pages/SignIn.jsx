53e4108f96d88a15df00893b01c7d35942039663
PaDuckk <kang10019@naver.com>
2019-04-05 15:22:07 +0900

    회원가입 페이지  페이지 구성 제작
    - 입력 가능하도록 Input의 state 와 event 추가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,19 +1,69 @@
 import React, { Component } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
 export default class SignIn extends Component {
+  state = {
+    emailInput: "",
+    passwordInput: "",
+    passwordConfirmInput: ""
+  };
+
+  onChangeEmailInput = e => {
+    this.setState({ emailInput: e.value });
+  };
+
+  onChangePasswordInput = e => {
+    this.setState({ passwordInput: e.value });
+  };
+
+  onChangePasswordConfirmInput = e => {
+    this.setState({
+      passwordConfirmInput: e.value
+    });
+  };
+
   render() {
+    const { emailInput, passwordInput, passwordConfirmInput } = this.state;
+    const {
+      onChangeEmailInput,
+      onChangePasswordInput,
+      onChangePasswordConfirmInput
+    } = this;
     return (
-      <Card className="text-center">
-        <Card.Header>Featured</Card.Header>
+      <Card>
+        <Card.Header>회원 가입</Card.Header>
         <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>이메일 주소</Form.Label>
+            <Form.Control
+              value={emailInput}
+              onChange={onChangeEmailInput}
+              type="email"
+              placeholder="Enter email"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>비밀번호</Form.Label>
+            <Form.Control
+              value={passwordInput}
+              onChange={onChangePasswordInput}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>비밀 번호 확인</Form.Label>
+            <Form.Control
+              value={passwordConfirmInput}
+              onChange={onChangePasswordConfirmInput}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            제출
+          </Button>
         </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
       </Card>
     );
   }
